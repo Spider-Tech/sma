b3b5ac10ecc37c4e5038f3b0e3901284aaa48801
Arun Pandian M <m.arunpandian@sudos.in>
2017-12-19 16:18:19 +0530

    adding .env

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv,find_dotenv
 app = Flask(__name__)
 
+load_dotenv(find_dotenv())
 
 @app.route('/')
 def hello():
     return "Hello World!"
+@app.route('/name')
+def name():
+	return os.environ.get("SECRET_KEY")
+
 
 if __name__ == '__main__':
     app.run()
